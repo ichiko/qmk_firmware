e2943be1353e8655f39263fb49d4d6e559f98b5c
ichiko <ichiko.revjune@gmail.com>
2019-10-13 13:45:59 +0900

    3rd version

--- keyboards/ergo42/keymaps/ichiko/keymap.c
+++ keyboards/ergo42/keymaps/ichiko/keymap.c
@@ -15,6 +15,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
    * ,------------------------------------------------.   ,------------------------------------------------.
+   * | ESC  |   W  |      |   1  |   2  |   3  |   4  |   |  Y   |   U  |   I  |   O  |   P  |   [  | Bksp |
+   * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
+   * |   A  |   S  |   D  |   5  |   6  |   7  |   8  |   |  H   |   J  |   K  |   L  |   ;  |   ]  | Enter|
+   * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
+   * |      |   F  |   T  |   9  |   0  |   -  |   '  |   |  N   |   M  |   ,  |   .  |   /  |  UP  | RSft |
+   * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
+   * | Tab  | Space|      |      |      | LCtrl| LSft |   |Space |   '  |   -  |   =  | LEFT | DOWN | RIGHT|
+   * `------------------------------------------------'   `------------------------------------------------'
+   */
+  [BASE] = LAYOUT( \
+    KC_ESC,  KC_W,   XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
+    KC_A,    KC_S,   KC_D,    KC_5,    KC_6,    KC_7,    KC_8,    KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RBRC, KC_ENT, \
+    XXXXXXX, KC_F,   KC_T,    KC_9,    KC_0,    KC_MINS, KC_QUOT,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_RSFT, \
+    KC_TAB,  KC_SPC, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_LSFT,  KC_SPC, KC_QUOT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
+  ),
+
+  /* BASE: 装飾キー左
+   * ,------------------------------------------------.   ,------------------------------------------------.
    * | ESC  |      |   W  |   F  |   1  |   2  |   3  |   |  Y   |   U  |   I  |   O  |   P  |   [  | Bksp |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | Tab  |   A  |   S  |   D  |   4  |   5  |   6  |   |  H   |   J  |   K  |   L  |   ;  |   ]  | Enter|
@@ -23,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * | LSft |      |      |      |   0  |   -  |   '  |   |Space |   '  |   -  |   =  | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
-   */
+   *
   [BASE] = LAYOUT( \
     KC_ESC,  XXXXXXX, KC_W,    KC_F,    KC_1,   KC_2,    KC_3,    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
     KC_TAB,    KC_A,  KC_S,    KC_D,    KC_4,   KC_5,    KC_6,    KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RBRC, KC_ENT, \
     KC_LCTL, XXXXXXX, KC_SPC,  KC_T,    KC_7,   KC_8,    KC_9,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_RSFT, \
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,   KC_MINS, KC_QUOT, KC_SPC, KC_QUOT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
-
+   */
 
   /* BASE: 親指移動がつらすぎた
    * ,------------------------------------------------.   ,------------------------------------------------.
