aa0d36332f5a0ed7444407e123d7f46518030e1e
ichiko <ichiko.revjune@gmail.com>
2020-05-02 21:45:34 +0900

    Update accessary keys

--- keyboards/ergo42/keymaps/ichiko/keymap.c
+++ keyboards/ergo42/keymaps/ichiko/keymap.c
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * |      |   F  |   T  |   9  |   0  |   -  |   '  |   |  N   |   M  |   ,  |   .  |   /  |  UP  | RSft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | Tab  | Space|      |      |      | LCtrl| LSft |   |Space |   '  |   -  |   =  | LEFT | DOWN | RIGHT|
+   * | Tab  | Space| Space|      | Alt  | LCtrl|      |   |Space |   '  |   -  |   =  | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
     KC_ESC,  KC_W,   XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
     KC_A,    KC_S,   KC_D,    KC_5,    KC_6,    KC_7,    KC_8,    KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RBRC, KC_ENT, \
     XXXXXXX, KC_F,   KC_T,    KC_9,    KC_0,    KC_MINS, KC_QUOT,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_RSFT, \
-    KC_TAB,  KC_SPC, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_LSFT,  KC_SPC, KC_QUOT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
+    KC_TAB,  KC_SPC, KC_SPC,  XXXXXXX, KC_LALT, KC_LCTL, XXXXXXX,  KC_SPC, KC_QUOT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
 
   /* BASE: 装飾キー左
